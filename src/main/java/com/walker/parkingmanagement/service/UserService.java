34333eca68aced508b891d499ead76963231e2a4
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-10-11 10:25:30 -0300

    Recurso criado para registrar um novo user

--- src/main/java/com/walker/parkingmanagement/service/UserService.java
+++ src/main/java/com/walker/parkingmanagement/service/UserService.java
@@ -1,11 +1,18 @@
 package com.walker.parkingmanagement.service;
 
+import com.walker.parkingmanagement.entity.User;
 import com.walker.parkingmanagement.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private UserRepository userRepository;
+
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
--- src/main/java/com/walker/parkingmanagement/web/controller/UserController.java
+++ src/main/java/com/walker/parkingmanagement/web/controller/UserController.java
@@ -1,13 +1,25 @@
 package com.walker.parkingmanagement.web.controller;
 
+import com.walker.parkingmanagement.entity.User;
 import com.walker.parkingmanagement.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(name = "api/v1/users")
+@RequestMapping("api/v1/users")
 public class UserController {
+
     private final UserService userService;
+
+    @PostMapping("api/v1/users")
+    public ResponseEntity<User> create(@RequestBody User user){
+        User userCreated = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
 }
