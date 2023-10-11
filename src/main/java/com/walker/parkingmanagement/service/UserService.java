928fe2df9ba48dc0b397eb4717c1ec5465b7e3b1
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-10-11 19:22:52 -0300

    Recurso criado para encontrar um user pelo id

--- src/main/java/com/walker/parkingmanagement/service/UserService.java
+++ src/main/java/com/walker/parkingmanagement/service/UserService.java
@@ -9,10 +9,15 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private UserRepository userRepository;
+
+    private final UserRepository userRepository;
 
     @Transactional
     public User save(User user) {
         return userRepository.save(user);
+    }
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(()-> new RuntimeException("Usuário não encontrado!"));
     }
 }
--- src/main/java/com/walker/parkingmanagement/web/controller/UserController.java
+++ src/main/java/com/walker/parkingmanagement/web/controller/UserController.java
@@ -5,10 +5,7 @@ import com.walker.parkingmanagement.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,9 +14,15 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("api/v1/users")
-    public ResponseEntity<User> create(@RequestBody User user){
+    @PostMapping("/create-user")
+    public ResponseEntity<User> createUser(@RequestBody User user){
         User userCreated = userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
+
+    @GetMapping("/find-user/{id}")
+    public ResponseEntity<User> findUserById(@PathVariable Long id){
+        User userFindById = userService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(userFindById);
     }
 }
