a1966d43fc471f9cb91c88fec720da421f2b55b4
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-10-31 13:28:54 -0300

    Implementação do primeiro teste: Criação de um novo user

--- src/test/java/com/walker/parkingmanagement/entity/UserTest.java
+++ src/test/java/com/walker/parkingmanagement/entity/UserTest.java
@@ -1,7 +1,11 @@
 package com.walker.parkingmanagement.entity;
 
+import com.walker.parkingmanagement.web.dto.CreateUserDTO;
+import com.walker.parkingmanagement.web.dto.ResponseUserDTO;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
@@ -13,4 +17,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserTest {
     @Autowired
     WebTestClient webTestClient;
+
+    @Test
+    public void testCreateUser_WithUsernameAndPasswordValid_ShouldReturnUserCreatedWithStatus201(){
+        ResponseUserDTO responseUserBodyDTO = webTestClient
+                .post()
+                .uri("/api/v1/users/create-user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateUserDTO("tody@email.com","123456"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(ResponseUserDTO.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getUsername()).isEqualTo("tody@email.com");
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getRole()).isEqualTo("CLIENT");
+    }
 }
