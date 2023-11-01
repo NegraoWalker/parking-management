7ca8e2b7e08123e96c67cf7c131f976444bdcdac
walkerNegrão <walker.esteves.negrao@gmail.com>
2023-11-01 08:08:15 -0300

    Implementado o teste de verificaçãoo: Eceções de validações de status 422:

--- src/test/java/com/walker/parkingmanagement/entity/UserTest.java
+++ src/test/java/com/walker/parkingmanagement/entity/UserTest.java
@@ -2,6 +2,7 @@ package com.walker.parkingmanagement.entity;
 
 import com.walker.parkingmanagement.web.dto.CreateUserDTO;
 import com.walker.parkingmanagement.web.dto.ResponseUserDTO;
+import com.walker.parkingmanagement.web.exception.ErrorMessage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,5 +35,89 @@ class UserTest {
         org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getId()).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getUsername()).isEqualTo("tody@email.com");
         org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getRole()).isEqualTo("CLIENT");
+    }
+
+    @Test
+    public void testCreateUser_WithUsernameInvalid_ShouldReturnErrorMessageWithStatus422(){
+        ErrorMessage responseUserBodyDTO = webTestClient
+                .post()
+                .uri("/api/v1/users/create-user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateUserDTO("","123456"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getStatus()).isEqualTo(422);
+
+        responseUserBodyDTO = webTestClient
+                .post()
+                .uri("/api/v1/users/create-user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateUserDTO("tody@","123456"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getStatus()).isEqualTo(422);
+
+        responseUserBodyDTO = webTestClient
+                .post()
+                .uri("/api/v1/users/create-user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateUserDTO("tody@email.","123456"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getStatus()).isEqualTo(422);
+    }
+
+    @Test
+    public void testCreateUser_WithPasswordInvalid_ShouldReturnErrorMessageWithStatus422(){
+        ErrorMessage responseUserBodyDTO = webTestClient
+                .post()
+                .uri("/api/v1/users/create-user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateUserDTO("tody@email.com",""))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getStatus()).isEqualTo(422);
+
+        responseUserBodyDTO = webTestClient
+                .post()
+                .uri("/api/v1/users/create-user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateUserDTO("tody@email.com","12345"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getStatus()).isEqualTo(422);
+
+        responseUserBodyDTO = webTestClient
+                .post()
+                .uri("/api/v1/users/create-user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateUserDTO("tody@email.com","1234567"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseUserBodyDTO.getStatus()).isEqualTo(422);
     }
 }
